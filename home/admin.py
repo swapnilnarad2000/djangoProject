f5ed97c5121ce8dde5373c39c05b634ee1921929
swapnil <swapniln@iitbhilai.ac.in>
2021-03-18 20:08:20 +0530

    some major changes

---
+++ home/admin.py
@@ -0,0 +1,7 @@
+from django.contrib import admin
+ #added start 
+from home.models import Contact
+# Register your models here.
+#Register your new app here
+admin.site.register(Contact)
+ #added start 
---
+++ home/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+#created by django after making home app
+class HomeConfig(AppConfig):
+    name = 'home'
